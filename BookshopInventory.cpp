6b15ca80033a9f58b9bb822ea042f57c6cdb6f08
James Blankenship <77518971+jbla484@users.noreply.github.com>
2021-09-25 13:52:37 -0700

    Update BookshopInventory.cpp

--- BookshopInventory.cpp
+++ BookshopInventory.cpp
@@ -3,49 +3,131 @@
 #include <vector>
 using namespace std;
 
-class Books {
-//Public member functions
+class Book {
+    //Public member functions
 public:
-    //Function to add a book to the vector
-    void addBook(string book) {
-        books.push_back(book);
-        ++count;
+    Book() {
+        this->bookName = "";
+        this->authorName = "";
+        this->publishDate = "";
     }
-    //Function to print all books in the vector
-    void printBooks() {
-        cout << "The books you entered: ";
-        for (size_t i = 0; i < books.size() - 1; i++)
-        {
-            cout << books[i] << ", ";
-        }
-        cout << books[books.size() - 1] << ".";
+    Book(string bookName, string authorName, string publishDate) {
+        this->bookName = bookName;
+        this->authorName = authorName;
+        this->publishDate = publishDate;
     }
-//Private data members
+    string GetBookName() {
+        return this->bookName;
+    }
+    void SetBookName(string bookName) {
+        this->bookName = bookName;
+    }
+    string GetAuthorName() {
+        return this->authorName;
+    }
+    void SetAuthorName(string authorName) {
+        this->authorName = authorName;
+    }
+    string GetPublishDate() {
+        return publishDate;
+    }
+    void SetPublishDate(string publishDate) {
+        this->publishDate = publishDate;
+    }
+    void print() {
+        cout << bookName << ", " << authorName << ", " << publishDate << "." << endl;
+    }
+    //Private data members
 private:
-    vector<string> books {};
-    int count = 0;
+    string bookName;
+    string authorName;
+    string publishDate;
 };
+
+class Books {
+public:
+    void SetBooks() {
+
+        string name, author, publishDate;
+
+        cin.ignore();
+        cout << "Enter the title: ";
+        getline(cin, name);
+        cout << "Enter the author: ";
+        getline(cin, author);
+        cout << "Enter the publish date: ";
+        getline(cin, publishDate);
+        Book example(name, author, publishDate);
+        books.push_back(example);
+        
+        return;
+    }
+    void DeleteBooks(string bookToDelete) {
+        for (size_t i = 0; i < books.size(); i++) {
+            if (bookToDelete == books.at(i).GetBookName()) {
+                books.erase(books.begin() + i);
+            }
+        }
+    }
+    void PrintBooks() {
+        for (size_t i = 0; i < books.size(); i++) {
+            books.at(i).print();
+        }
+    }
+private:
+    vector<Book> books;
+
+};
+
+void WelcomeMessage() {
+
+    //Intro message
+    cout << "Welcome to James' book inventory system!" << endl;
+    cout << "Press 'q' to exit the program." << endl << endl;
+
+    cout << "To add a book into inventory, enter 'a'" << endl;
+    cout << "To remove a book from inventory, enter 'r'" << endl;
+    cout << "To print the inventory, enter 'p'" << endl << endl;
+
+}
 
 int main()
 {
     string book;
     char response;
-    Books currentBook;
-    //Intro message
-    cout << "Welcome to James' book inventory system!" << endl << endl;
-    //Looping until the user enters 'n'
-    do
-    {
-        //Getting input for book, then adding to the vector via function call to addBook()
-        cout << "Enter a book into inventory: " << endl;
-        getline(cin, book);
-        currentBook.addBook(book);  
-        //Asking if the user wants to continue to add books
-        cout << "\nDo you want to add another one? ( y / n )" << endl;
-        cin >> response;
-        cin.ignore();
+    Books theBooks;
 
-    } while (response != 'n');
-    //Function call to print all the books
-    currentBook.printBooks();
+    WelcomeMessage();
+
+    cin >> response;
+
+    while (response != 'q') {
+
+        if (response == 'a') {
+
+            theBooks.SetBooks();
+
+            cout << endl << "Enter another command: ";
+            cin >> response;
+            
+        }
+        else if (response == 'r') {
+
+            cout << "Enter the book name: ";
+            cin >> book;
+
+            theBooks.DeleteBooks(book);
+
+            cout << endl << "Enter another command: ";
+            cin >> response;
+        }
+        else if (response == 'p') {
+
+            theBooks.PrintBooks();
+
+            cout << endl << "Enter another command: ";
+            cin >> response;
+        }
+
+    }
 }
